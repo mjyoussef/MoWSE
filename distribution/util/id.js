7ebcceef85c77b94a35c3835342820cc462bb3dc
Michael Youssef <michael_youssef@brown.edu>
2024-04-15 18:54:17 -0400

    all.mem

--- distribution/util/id.js
+++ distribution/util/id.js
@@ -21,8 +21,65 @@ function getSID(node) {
   return getNID(node).substring(0, 5);
 }
 
+// used for hash function
+function idToNum(id) {
+  let n = parseInt(id, 16);
+  assert(!isNaN(n), 'idToNum: id is not in KID form!');
+  return n;
+}
+
+// naive hash function
+function naiveHash(kid, nids) {
+  nids.sort();
+  return nids[idToNum(kid) % nids.length];
+}
+
+// consistent hash function
+function consistentHash(kid, nids) {
+  let lst = [[idToNum(kid), kid]];
+  for (let i=0; i<nids.length; i++) {
+    lst.push([idToNum(nids[i]), nids[i]]);
+  }
+
+  lst.sort((e1, e2) => {
+    return e1[0] - e2[0];
+  });
+
+  for (let i=0; i<lst.length; i++) {
+    if (lst[i][1] === kid) {
+      let next = i+1;
+      if (next === lst.length) {
+        next = 0;
+      }
+      return lst[next][1];
+    }
+  }
+}
+
+// rendezvous hash function
+function rendezvousHash(kid, nids) {
+  let lst = [];
+  for (let i=0; i<nids.length; i++) {
+    const concatenation = kid + nids[i];
+    const hash = getID(concatenation);
+    lst.push([idToNum(hash), nids[i]]);
+  }
+
+  lst.sort((e1, e2) => {
+    return e1[0] - e2[0];
+  });
+
+  console.log(lst);
+  return lst[lst.length-1][1];
+}
+
+
 module.exports = {
   getNID: getNID,
   getSID: getSID,
   getID: getID,
+  idToNum: idToNum,
+  naiveHash: naiveHash,
+  consistentHash: consistentHash,
+  rendezvousHash: rendezvousHash,
 };
