51f7e83c13b2866a7c068f4f2389fc1ab8f2abf6
mdlu02 <michaeldavidlu@gmail.com>
2024-04-17 06:30:58 +0000

    update

--- distribution/indexer/indexer.js
+++ distribution/indexer/indexer.js
@@ -8,7 +8,7 @@ const index_mr = (config) => {
     index: (data, callback) => {
 
       const doMapReduce = (cb) => {
-        mr_args = {mrid: 'index-mr', mapFn: mr.map, reduceFn: mr.reduce};
+        mr_args = {mrid: 'index_mr', mapFn: mr.map, reduceFn: mr.reduce};
         distribution[gid].mr.exec(mr_args, (e, v) => {
           if (e) {
             cb(e, null);
@@ -16,11 +16,13 @@ const index_mr = (config) => {
           v.forEach((o) => {
             let url = Object.keys(o)[0];
             let embedding = o[key];
-            distribution[gid].store.put(embedding, url, (e, v) => {
+            distribution[gid].store.vecput(embedding, url, (e, v) => {
               if (e) {
                 cb(e, null);
+              } else {
+                cb(null, v);
               }
-            }, ['index_mr', 'results']);
+            });
           });
         });
       };
@@ -31,9 +33,14 @@ const index_mr = (config) => {
         let key = Object.keys(o)[0];
         let value = o[key];
         distribution[gid].store.put(value, key, (e, v) => {
-          cnt++;
-          if (cntr === dataset.length) {
-            doMapReduce(callback);
+          if (e) {
+            callback(e, null);
+          } else {
+            cnt++;
+            if (cnt === data.length) {
+              doMapReduce(callback);
+            }
+          
           }
         }, ['index_mr', 'map']);
       });
