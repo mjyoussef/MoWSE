5e0d80728b6d82bfa92dd340d8d6ef2d54ef9ff7
Michael Youssef <michael_youssef@brown.edu>
2024-05-02 16:09:22 -0400

    jsdocs for distribution.all

--- distribution/all/status.js
+++ distribution/all/status.js
@@ -1,16 +1,16 @@
-/* Checks the status of every node in the group.
-
-PARAMETERS:
-context: stores the group ID
-configuration: field to check (ie. SID, memory usage, etc)
-callback: an optional callback
-*/
-const get = (context, configuration, callback) => {
-  callback = callback || function(e, v) {};
-  const message = [configuration];
-  const remote = {service: 'status', method: 'get'};
+/**
+ * Gets status for some node attribute (ie. memory usage, sid, nid, etc).
+ *
+ * @param {Object} context - metadata such as gid
+ * @param {string} attribute - attribute name
+ * @param {Function} callback - optional callback that accepts error, value
+ */
+const get = (context, attribute, callback) => {
+  callback = callback || function (e, v) {};
+  const message = [attribute];
+  const remote = { service: "status", method: "get" };
   global.distribution[context.gid].comm.send(message, remote, (e, v) => {
-    if (Object.keys(e).length === 0 && configuration === 'heapTotal') {
+    if (Object.keys(e).length === 0 && attribute === "heapTotal") {
       const heapTotal = Object.values(v).reduce((sum, value) => sum + value, 0);
       callback(e, heapTotal);
     } else {
@@ -19,42 +19,42 @@ const get = (context, configuration, callback) => {
   });
 };
 
-/* Stops all nodes in the group.
-
-PARAMETERS:
-context: stores the group ID
-callback: an optional callback
-*/
+/**
+ * Gracefully stops all nodes.
+ *
+ * @param {Object} context - metadata such as gid
+ * @param {Function} callback - optional callback that accepts error, value
+ */
 const stop = (context, callback) => {
-  const remote = {service: 'status', method: 'stop'};
+  const remote = { service: "status", method: "stop" };
   global.distribution[context.gid].comm.send([], remote, (e, v) => {
     global.distribution.local.status.stop(callback);
   });
 };
 
-/* Spawns a node in the group.
-
-PARAMETERS:
-context: stores the group ID
-nodeConfig: node configuration
-callback: an optional callback
-*/
+/**
+ * Spawns a new node and adds it to the group.
+ *
+ * @param {Object} context - metadata such as gid
+ * @param {Object} nodeConfig - configuration for the node (ie. ip, port, etc)
+ * @param {Function} callback - optional callback that accepts error, value
+ */
 const spawn = (context, nodeConfig, callback) => {
   global.distribution.local.status.spawn(nodeConfig, (e1, v1) => {
     global.distribution[context.gid].groups.add(
-        context.gid,
-        nodeConfig,
-        (e2, v2) => {
-          if (Object.keys(e2).length === 0) {
-            callback(null, nodeConfig);
-          } else {
-            callback(
-                new Error(
-                    `could not spawn node ${nodeConfig.ip}:${nodeConfig.port}`,
-                ),
-            );
-          }
-        },
+      context.gid,
+      nodeConfig,
+      (e2, v2) => {
+        if (Object.keys(e2).length === 0) {
+          callback(null, nodeConfig);
+        } else {
+          callback(
+            new Error(
+              `could not spawn node ${nodeConfig.ip}:${nodeConfig.port}`
+            )
+          );
+        }
+      }
     );
   });
 };
@@ -62,7 +62,7 @@ const spawn = (context, nodeConfig, callback) => {
 let status = (config) => {
   let context = {};
 
-  context.gid = config.gid || 'all';
+  context.gid = config.gid || "all";
 
   return {
     get: get.bind(null, context),
