9b2d9aa7b6027bb7aa2ef34805bb38ed3000b904
Neil Xu <xu3033@gmail.com>
2024-05-02 12:42:17 -0400

    adding cloud boothook

--- engine/local/testCrawler.js
+++ engine/local/testCrawler.js
@@ -144,13 +144,13 @@ if (require.main === module) {
   }
 
   const startScript = `
+  #cloud-boothook
   #!/bin/bash
   sudo yum install -y git
   git clone -b deployment https://github.com/mjyoussef/mowse.git
   sudo yum install nodejs -y
   cd mowse
   npm install
-
   node distribution.js --ip "0.0.0.0" --port 7070
   `;
 
